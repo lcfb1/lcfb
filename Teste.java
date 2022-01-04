1ed0b979e8d711b56df7e1b41328e0597068bc46
lcfb1 <leticia.cfb@gmail.com>
2022-01-04 15:46:25 +0000

    Aula 0
    Criado ficheiro teste.java classe teste e função soma

---
+++ Teste.java
@@ -0,0 +1,23 @@
+package Teste;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class teste {
+
+	public static void main(String[] args) 
+	{
+	
+
+	    Scanner sc = new Scanner(System.in);
+	   
+	    System.out.println(" Escreve dois numeros");
+	    
+	    int a = sc.nextInt();
+	    
+	    int b = sc.nextInt();
+	    
+	     
+	    System.out.println( a + b);
+	    sc.close();
+}}
